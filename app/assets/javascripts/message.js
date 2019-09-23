48017051e8f3a59c089f5255aa92d903f7618aa9
鈴木達也 <tat@suzukitatsuyanoMacBook-Air.local>
2019-09-23 20:19:58 +0900

    再修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,8 @@
 $(function(){ 
   function buildHTML(message){
-   if ( message.image ) {
+
+  var img  = message.image != null ?`<img src=${message.image} >` : ''
+
      var html =
       `<div class="message" data-message-id=${message.id}>
          <div class="upper-message">
@@ -16,29 +18,10 @@ $(function(){
              ${message.content}
            </p>
          </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
-   } else {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.date}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
+          ${img}
        </div>`
      return html;
    };
- }
 $('#new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
@@ -52,7 +35,6 @@ $('#new_message').on('submit', function(e){
    contentType: false
  })
   .done(function(data){
-    console.log(data);
     var html = buildHTML(data);
     $('.messages').append(html);
     $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   

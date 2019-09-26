67adfc34ed3af45680f9674dd292934b147650fc
鈴木達也 <tat@suzukitatsuyanoMacBook-Air.local>
2019-09-26 20:38:25 +0900

    t

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.1'
 set :application, 'cat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:<tat0820>/<cat-space>.git'
+set :repo_url,  'git@github.com:tat0820/cat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
